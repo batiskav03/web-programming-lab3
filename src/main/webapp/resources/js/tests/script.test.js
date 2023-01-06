cff5e75451ae48a395318b90ba201d4e319e0e78
Maxim Kalabukhov <338956@niuitmo.ru>
2023-01-06 17:16:50 +0300

    fixed copy of code

--- src/main/webapp/resources/js/tests/script.test.js
+++ src/main/webapp/resources/js/tests/script.test.js
@@ -1,8 +1,22 @@
-const WebSocket = require("ws")
 const {requestOnServer} = require("../input/requestOnServer");
 
 
 describe("requestOnServer", () => {
+    let bigData;
+    let socket;
+    beforeEach(() => {
+        bigData = [];
+        socket = {send: jest.fn(() => {
+                return socket.onmessage("[[269,538],[318,604]]")
+        }),
+            onmessage: function (msg) {
+                let arr = JSON.parse(msg)
+                for (str of arr) {
+                    bigData.push(str)
+                }
+            },
+        }
+    })
     test("should call socket.send", () => {
         const socket = {send: jest.fn()}
         requestOnServer(socket,[],1,1, new Date())
@@ -10,61 +24,22 @@ describe("requestOnServer", () => {
     })
 
     test("array should stay Truthy", () => {
-        let bigData = [];
-        const socket = {send: jest.fn(() => {
-                                                             return socket.onmessage("[[269,538],[318,604]]")}),
-                        onmessage: function (msg) {
-                            let arr = JSON.parse(msg)
-                            for (str of arr) {
-                                bigData.push(str)
-                            }
-                        },
-        }
         requestOnServer(socket,bigData, 1, 1400, new Date())
         expect(bigData).toBeTruthy()
 
     })
     test("should append values into array", () => {
-        let bigData = [];
-        const socket = {send: jest.fn(() => {
-                return socket.onmessage("[[269,538],[318,604]]")}),
-            onmessage: function (msg) {
-                let arr = JSON.parse(msg)
-                for (str of arr) {
-                    bigData.push(str)
-                }
-            },
-        }
         requestOnServer(socket,bigData, 1, 1400, new Date())
         expect(bigData.length).toBe(2)
 
     })
     test("should append values into array", () => {
-        let bigData = [];
-        const socket = {send: jest.fn(() => {
-                return socket.onmessage("[[269,538],[318,604]]")}),
-            onmessage: function (msg) {
-                let arr = JSON.parse(msg)
-                for (str of arr) {
-                    bigData.push(str)
-                }
-            },
-        }
         requestOnServer(socket,bigData, 1, 1400, new Date())
         expect(bigData).toEqual([[269,538],[318,604]])
 
     })
     test("should append values into array", () => {
-        let bigData = [[1,2]];
-        const socket = {send: jest.fn(() => {
-                return socket.onmessage("[[269,538],[318,604]]")}),
-            onmessage: function (msg) {
-                let arr = JSON.parse(msg)
-                for (str of arr) {
-                    bigData.push(str)
-                }
-            },
-        }
+        bigData = [[1,2]]
         requestOnServer(socket,bigData, 1, 1400, new Date())
         expect(bigData).toEqual([[1,2]])
 
